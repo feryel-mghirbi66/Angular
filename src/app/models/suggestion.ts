0127d70d186be8dd35b8fe7cd880b2aa6c1642bb
eya mghirbi <Feryel.Mghirbi@esprit.tn>
2026-02-25 22:32:42 +0100

    workshop 2 et 3

---
+++ src/app/models/suggestion.ts
@@ -0,0 +1,13 @@
+export class Suggestion {
+    
+        id!: number;
+        title!: string;
+        description!: string;
+        category!: string;
+        date!: Date;
+        status!: string;
+        nbLikes!: number;
+      
+      
+}
+
